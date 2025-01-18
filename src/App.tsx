3c81f6df99c29f34f3f8b64ee81c2488bda28122
David von Wrangel <wrangelvid@gmail.com>
2025-01-17 22:07:23 -0500

    r3 handle flips the world

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,112 @@
-import { useRef } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
+import { Canvas, useThree } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
-import { type Mesh } from "three";
+import { OrbitHandles } from "@react-three/handle";
+import { Vector3 } from "three";
 import {
   createXRStore,
-  IfInSessionMode,
   noEvents,
   PointerEvents,
   XR,
+  XROrigin,
   XRStoreOptions,
 } from "@react-three/xr";
-import { Fullscreen } from "@react-three/uikit";
-import { EnterXRButton } from "./EnterXRButton";
+import { Container, Fullscreen, Text } from "@react-three/uikit";
+import { useComputed, useSignal } from "@preact/signals-react";
 
-function SpinningCube() {
-  const cubeRef = useRef<Mesh>(null);
 
-  // Animate rotation using useFrame
-  useFrame(() => {
-    if (cubeRef.current) {
-      cubeRef.current.rotation.x += 0.01;
-      cubeRef.current.rotation.y += 0.01;
-    }
-  });
 
+export default function App() {
+  const store = createXRStore(xrOptions);
   return (
-    <mesh ref={cubeRef}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color="yellow" />
-    </mesh>
+    <Canvas
+      events={noEvents}
+    
+      camera={{ position: [2, -2, 2] }}
+    >
+      <XR store={store}>
+        <PointerEvents batchEvents={false} />
+        <XROrigin rotation-x={Math.PI / 2} />
+
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[5, 5, 5]} />
+
+        <Triad />
+        <OrbitHandleSwitch />
+      </XR>
+    </Canvas>
   );
 }
 
-export default function App() {
-  const options: XRStoreOptions = {
+function OrbitHandleSwitch() {
+  const camera = useThree((state) => state.camera);
+  const controlMode = useSignal("nothing");
+  const buttonText = useComputed(() => {
+    switch (controlMode.value) { 
+      case "nothing":
+        return "Switch to @react-three/drei OrbitControls";
+      case "drei":
+        return "Switch to @react-three/handle OrbitControls";
+      case "handle":
+        return "Switch to no controls."
+    }});
+  return (
+    <>
+      <Fullscreen flexDirection="row" justifyContent="flex-start" alignItems="flex-start" padding={20}>
+        <Container flexDirection="column" gap={20} backgroundColor="lightgray" padding={20}>
+          <Text>The Blue Axis (z) should point up!</Text>
+          <Container
+            backgroundColor="lightblue"
+            hover={{ backgroundColor: "orange" }}
+            padding={20}
+            borderRadius={20}
+            onClick={() => {
+              switch (controlMode.value) {
+                case "nothing":
+                  controlMode.value = "drei";
+                 break;
+                case "drei":
+                  controlMode.value = "handle";
+                  break;
+                case "handle":
+                  controlMode.value = "nothing";
+
+                  // Reset the camera.
+                  camera.position.set(2, -2, 2);
+                  camera.lookAt(new Vector3(0, 0, 0));
+                  break;
+              }
+          }}
+          >
+            <Text>{buttonText}</Text>
+          </Container>
+        </Container>
+      </Fullscreen>
+      {controlMode.value === "drei" && <OrbitControls />}
+      {controlMode.value === "handle" && <OrbitHandles />}
+    </>
+  );
+}
+
+function Triad() {
+  return (
+    <group>
+      <mesh position-x={0.6}>
+        <boxGeometry args={[1.0,0.2,0.2]}/>
+        <meshBasicMaterial color="red" />
+      </mesh>
+      <mesh position-y={0.6}>
+        <boxGeometry args={[0.2,1,0.2]}/>
+        <meshBasicMaterial color="green" />
+      </mesh>
+      <mesh position-z={0.6}>
+        <boxGeometry args={[0.2,0.2,1]}/>
+        <meshBasicMaterial color="blue" />
+      </mesh>
+    </group>
+  )
+}
+
+  const xrOptions: XRStoreOptions = {
     handTracking: true,
     foveation: 0,
     domOverlay: false,
@@ -76,29 +149,3 @@ export default function App() {
       teleportPointer: false,
     },
   };
-  const store = createXRStore(options);
-  return (
-    <Canvas events={noEvents}>
-      <XR store={store}>
-        <PointerEvents batchEvents={false} />
-        <ambientLight intensity={0.5} />
-        <directionalLight position={[5, 5, 5]} />
-        <SpinningCube />
-        <OrbitControls />
-        <IfInSessionMode deny={["immersive-ar", "immersive-vr"]}>
-          <Fullscreen
-            flexDirection="row"
-            padding={20}
-            paddingRight={50}
-            alignItems="flex-start"
-            justifyContent="flex-end"
-            pointerEvents="listener"
-            pointerEventsOrder={3}
-          >
-            <EnterXRButton />
-          </Fullscreen>
-        </IfInSessionMode>
-      </XR>
-    </Canvas>
-  );
-}
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { Object3D, Vector3 } from 'three';
+
+Object3D.DEFAULT_UP = new Vector3(0, 0, 1);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
